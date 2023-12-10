015d95e2ec70fccca137d01fc374109c1869cfea
viniciusdutra314 <dutravinisousa2@usp.br>
2023-12-10 20:34:46 -0300

    f-strings and refactoring
    not yet working

--- CardMaker.py
+++ CardMaker.py
@@ -1,131 +1,126 @@
-from rich import print #insert colors and emojis on the terminal
-import genanki #integration with anki
-from googletrans import Translator #translate the phrases
-from openpyxl import load_workbook,Workbook
-from gtts import gTTS #generate audios
-import os #get the file directories
-import time #count the amount of time used in creating cards
-import tempfile #save and delete the tempaudios
+from os import listdir,getcwd 
+from tempfile import TemporaryDirectory  
+from time import time
 
-translator = Translator() #start translator
-t_o=time.time() # counting time
+import genanki  
+from googletrans import Translator,LANGUAGES
+from gtts import gTTS  
+from openpyxl import Workbook, load_workbook
+from rich import print  
 def translate(message,source="en"):
   global chosen_language
   return translator.translate(message, src=source,dest=chosen_language).text
+def import_column(table,column):
+  return [cell.value for cell in table[column] if cell.value][1::]
+def insert_sound(file_name): return f"[sound:{file_name}.mp3]"
+def bold(text): return f'<u><b><i>{text}</i></b></u></span> '
+def create_note(text):
+    global i,my_model,langs
+    return genanki.Note(model=my_model,fields=text,
+                        tags=[str(langs[int(i)]), "cardmaker"])
+def get_user_input(prompt,validation_func,
+                   erro_msg='',input_mod=lambda x:x):
+   while True:
+      user_input=input(prompt)
+      if validation_func(prompt):
+            return input_mod(user_input)
+      else:
+         print(erro_msg)
+def is_lang_valid(language_code):
+    return language_code in LANGUAGES.keys()
+def chatgpt_copy_n_past(cardtype,word,phrase):
+    if cardtype=="v":
+      prompt=translate("Explain what this word")
+      prompt+=f"_{word}_"
+      prompt+=translate("means in this phrase and also in general")
+      prompt+=f"_{phrase}_"
+      return prompt 
+    else:
+      return translate(f"Translate this phrase to my mother 
+                       tongue and explain is meaning") +phrases[i]         
+translator = Translator() 
+t_o=time() 
 
 print('Welcome to Anki-Cardmaker :earth_americas: !')
-print('The program is on a command line but there is no need to fear :smiley:')
-
-chosen_language=input("Choose the language that your cards and the interface will be translated (ex:pt,fr,en): ").lower()
-while True:
-  try:
-    translate("checking if the chosen_language is actually working")
-    break
-  except: 
-    print("You have typed your language [red] wrongly [/red]or unfortunately your language is not supported by the code :(")
-    chosen_language=input("Choose the language that your cards and the interface will be translated (ex:pt,fr,en): ").lower()
-tables_in_the_directory=[j for j in os.listdir() if j[-5::]==".xlsx" and j[0:2]!="~$" and j not in ["checktable.xlsx","verifiedtable.xlsx"]] 
-
-if len(tables_in_the_directory)>1:  #multiple tables to select
+print(f'The program is on a command line but there is 
+      no need to fear :smiley:')
+chosen_language=get_user_input(f"Choose the language that your cards and 
+                          the interface will be translated (ex:pt,fr,en): ",
+                          lambda lang: is_lang_valid(lang),
+                          f"Typo in your language or unfortunately your language 
+                          is not supported by the code :(",
+                          lambda x:x.lower())
+  
+tables_in_the_directory=[j for j in listdir() if j.endswith('.xlsx') and j.startswith("~$") 
+                         and j not in ["temp_table.xlsx","verified_table.xlsx"]] 
+num_tables=len(tables_in_the_directory)
+if len(num_tables)==0:
+   print(translate("You haven't import any excel table"))
+elif len(num_tables)==1:
+   table_name=tables_in_the_directory
+elif len(num_tables)>1: 
   print(tables_in_the_directory)
-  while True:
-    table_name=input(translate("Name of the excel table")+" ")+".xlsx"
-    if table_name in tables_in_the_directory: break
-    else:
-      wrong_name=translate("Wrong name!")
-      print('[red]wrong_name[/red]')
-else: 
-  try: table_name=tables_in_the_directory[0]
-  except: raise ValueError(translate("You haven't import any excel table"))
-  print(translate("Using the table")+"[green] "+table_name+"[/green]")
-workbook1 ,deck_name= load_workbook(table_name), table_name[:-5:]
-table=workbook1.active
-print("[blue]p=pronunciation [/blue] [red]s=speaking [/red] [green] w=writing[/green] [yellow] v=vocabulary [/yellow]")
-cardtype=input(translate("What is the type of your card?")+" ").lower()
-while cardtype not in ["s","p","w","v"]:
-  print(translate("[red]ERROR[/red], cardtype invalid, type it again"))
-  cardtype=input()
+  table_name=get_user_input(translate("Name of the excel table"),
+            lambda table_name: table_name+".xlsx" in tables_in_the_directory,
+            translate("Invalid table name"),
+            lambda x: x +".xlsx")
+  
+print(f"{translate('Using the table')}[green]table_name[/green]")
+workbook ,deck_name= load_workbook(table_name), table_name[:-5:]
+table=workbook.active
+print(f"[blue]p=pronunciation [/blue] [red]s=speaking [/red] 
+      [green] w=writing[/green] [yellow] v=vocabulary [/yellow]")
+cardtype=get_user_input(translate("What is the type of your card?"),
+                   lambda cardtype: cardtype in ["s","p","w","v"],
+                   translate("[red]ERROR[/red], cardtype invalid"),
+                   lambda x:x[0].lower())
 
-def import_column(table,column):
-  return [cell.value for cell in table[column] if cell.value!=None and cell.value!=""][1::]
 
 phrases = import_column(table,"A")
 if cardtype=="v": 
-  words=import_column(table,"B")
-
-column="B" if cardtype!="v"  else "C"
-languages=import_column(table,column)
-if len(languages)==1:
-  unique_lang=languages[0]
-  languages=[unique_lang for _ in range(len(phrases))]
-assert len(languages)>1, translate("Language error, you probably forgot to specify the languages in your excel table")
-
-workbook1.close()
-
-def wordinphrase(word,phrase):
-    word,phrase=str(word).lower(),str(phrase).lower()
-    remove=[".",",","?","!",":",";","-","=","_"]
-    for k in remove:
-        word=word.replace(k,"")
-        phrase=phrase.replace(k," ")
-    if len(word.split())==1: #single word
-      for j in phrase.split():
-          if str(j)==str(word):
-              return True
-    else: #composite word
-      if word in phrase: return True
-    return False
-
-if cardtype=='v':
-    if input(translate("Do you want to check if the word is in the phrase?")+" y=yes, n=no:")[0].lower() =="y":
-        for j in range(len(phrases)):
-            if not wordinphrase(words[j],phrases[j]):
-              while True:
-                print(translate("[red]Error[/red]: Word not in the phrase"))
-                print("\n"+phrases[j]+"\n"+words[j])
-                words[j]=input(translate("Correct word:"))
-                if wordinphrase(words[j],phrases[j]):
-                  break
-
+   words=import_column(table,"B")
+   langs=import_column(table,'C')
+else:
+  langs=import_column(table,'B')
+workbook.close()
+if len(langs)==1:
+  unique_lang=langs[0]
+  langs=[unique_lang for _ in range(len(phrases))]
+for line,lang in enumerate(langs): 
+   if not is_lang_valid(lang):
+    raise ValueError(f"{lang} in line {line} is not supported")
 
 print(translate("Translating...(1/2)"))
-translated_phrases=[translate(phrases[i],source=languages[i]) for i in range(len(phrases))]
-if cardtype=="v": translated_words=[translate(words[i],source=languages[i]) for i in range(len(phrases))]
+translated_phrases=[translate(phrases[i],source=langs[i]) for i in range(len(phrases))]
+if cardtype=="v": translated_words=[translate(words[i],source=langs[i]) for i in range(len(phrases))]
 
 #checking
 if cardtype!="p":
   workbook2= Workbook()
   check_table=workbook2.active
-  vocab_header=["Words","Translated_Words","ChatGPT"] if cardtype=="v" else ["ChatGPT"]
+  vocab_header=["Words","Translated_Words"] 
+  if cardtype=="v": vocab_header.append("ChatGPT")
   check_table.append(["Phrases","Translated_Phrases"]+ vocab_header)
-
-  def chatgpt_copy_n_past(i):
-    global cardtype,words,phrases
-    if cardtype=="v":
-      return translate("Explain what this word") +f"_{words[i]}_" + translate("means in this phrase and also in general") + f"_{phrases[i]}_"
-    else:
-      return translate("Translate this phrase to my mother tongue and explain is meaning") +phrases[i]
-
   for i in range(len(phrases)):
     check_table["A"+str(i+2)]=phrases[i]
     check_table["B"+str(i+2)]=translated_phrases[i]
-    check_table["C"+str(i+2)]=words[i] if cardtype=="v" else chatgpt_copy_n_past(i)
-    if cardtype=="v":
+    if cardtype!="v":
+      check_table["C"+str(i+2)]=chatgpt_copy_n_past(cardtype,
+                                                    words[i],phrases[i])
+    else:  
+      check_table["C"+str(i+2)]=words[i]
       check_table["D"+str(i+2)]=translated_words[i]
-      check_table["E"+str(i+2)]=chatgpt_copy_n_past(i)
-  workbook2.save("checktable.xlsx")
+      check_table["E"+str(i+2)]=chatgpt_copy_n_past(cardtype,
+                                                    words[i],phrases[i])
+  workbook2.save("check_table.xlsx")
   workbook2.close()
-  print(translate("Your translations are ready in")+"[green] checktable.xlsx! [/green]")
+  print(f"{translate('Your translations are ready in')}[green] check_table.xlsx![/green]")
   print(translate("Please assert that everthing is fine and then save as"))
-  print("[green] verifiedtable.xlsx [/green]"+translate(",press any key to continue"))
-  time_to_check=input()
-  while True:
-    try:
-      workbook3=load_workbook("verifiedtable.xlsx")
-      break
-    except: 
-      print(translate("[red]Error, You forgot to save the verifiedtable.xlsx [/red]"))
-      time_to_check=input(translate("press any key to continue"))
+  input(f"[green] verified_table.xlsx [/green],{translate('press any key to continue')}:")
+  while 'verified_table.xlsx' not in listdir():
+      print(f"[red]translate('Error, You forgot to save the verified_table.xlsx')[/red]")
+      input(translate("press any key to continue"))
+  workbook3=load_workbook("verified_table.xlsx")
   verified_table=workbook3.active  
   phrases= import_column(verified_table,"A")
   translated_phrases=import_column(verified_table,"B")
@@ -135,21 +130,21 @@ if cardtype!="p":
   workbook3.close()
 print(translate("Audios...(2/2)"))
 #create a temp directory inside the project's folder
-with tempfile.TemporaryDirectory(dir=os.getcwd(),prefix=".tempaudio") as audio_path:
+with TemporaryDirectory(dir=getcwd(),prefix=".tempaudio") as audio_path:
   for j in range(len(phrases)):
       try:
         if cardtype=="v":
-          audio = gTTS(words[j]+"."+phrases[j], lang=languages[j])
-          audio.save(audio_path + "//" + deck_name + "phrase" + str(j) + '.mp3')
-          audio= gTTS(words[j], lang=languages[j])
-          audio.save(audio_path + "//" + deck_name + "word" + str(j) + '.mp3')
-        if cardtype in ["s","w"]:
-          audio = gTTS(phrases[j], lang=languages[j])
-          audio.save(audio_path + "//" + deck_name + "phrase" + str(j) + '.mp3')
-        if cardtype=="p":
-          audio = gTTS(phrases[j], lang=languages[j])
-          audio.save(audio_path + "//" + deck_name + "pronunciation" + str(j) + '.mp3')
-      except:pass
+          audio_phrase = gTTS(f"{words[j]}.{phrases[j]}", lang=langs[j])
+          audio_phrase.save(f"{audio_path}//{deck_name}phrase{j}.mp3")
+          audio_word= gTTS(words[j], lang=langs[j])
+          audio_word.save(f"{audio_path}//{deck_name}word{j}.mp3")
+        elif cardtype in ["s","w"]:
+          audio_phrase = gTTS(phrases[j], lang=langs[j])
+          audio_phrase.save(f"{audio_path}//{deck_name}phrase{j}.mp3")
+        elif cardtype=="p":
+          audio = gTTS(phrases[j], lang=langs[j])
+          audio.save(f"{audio_path}//{deck_name}pronunciation{j}.mp3")
+      except:...
 
   from random import randint
   id_deck =randint(1e9, 1e10)
@@ -169,26 +164,26 @@ with tempfile.TemporaryDirectory(dir=os.getcwd(),prefix=".tempaudio") as audio_p
                   'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
               },
           ])
-  if cardtype=="w":
+  elif cardtype=="w":
       my_model = genanki.Model(
           516_000_134,
           'CardMaker Type in the Answer',
           fields=fields,
           templates=[
               {
-                  'name': 'Card 1',
+                  'name': 'Card Type in the Answer',
                   'qfmt': '{{Question}}<br> {{type:Answer}}',
                   'afmt': '{{FrontSide}}{{MyMedia}}',
               },
           ])
-  if cardtype=="p":
+  elif cardtype=="p":
       my_model = genanki.Model(
           130_931_301,
           'CardMaker Pronunciation',
           fields=fields,
           templates=[
               {
-                  'name': 'Card 1',
+                  'name': 'Card Pronunciation',
                   'qfmt': '{{Question}}<br>{{MyMedia}}',
                   'afmt': '{{FrontSide}}<hr id="answer">{{Answer}}',
               },
@@ -196,43 +191,41 @@ with tempfile.TemporaryDirectory(dir=os.getcwd(),prefix=".tempaudio") as audio_p
   #Color of the cards
 
   print("[blue]Blue[/blue] [red]Red [/red] [yellow]Yellow[/yellow] [magenta] Purple [/magenta] [green] Green [/green]")
-  color=input(translate("Select a color: "))[0:2].lower()
   colors_rgb = {'gr': '<span style="color: rgb(81, 255, 37);">', 're': '<span style="color: rgb(228, 14, 14);">',
                     'bl': '<span style="color: rgb(18, 166, 252);">',
                     'ye': '<span style="color: rgb(249, 255, 54);">',
                     'pu': '<span style="color: rgb(198, 38, 255);">',
                     'pi': '<span style="color: rgb(255, 14, 192);">', }
-  while True:
-    try:
-      color=colors_rgb[color]
-      break
-    except:
-      print(translate("[red]Color invalid! [/red]"))
-      color=input(translate("Select a color: "))[0:2].lower()
-
-  def insert_sound(file_name): return "[" + "sound:" + file_name + ".mp3" + "]"
-  def bold(text): return '<u><b><i>' + text +'</i></b></u></span> '
-  def create_note(text):
-    global i,my_model,languages
-    return genanki.Note(model=my_model,fields=text,tags=[str(languages[int(i)]), "cardmaker"])
+  color=get_user_input(translate("Select a color: "),
+                  lambda color: colors_rgb[color],
+                  translate("[red]Color invalid! [/red]"),
+                  lambda color:color[0:2].lower())
 
   for i in range(len(phrases)):
         try:
           if cardtype=="s":
-              note =create_note([color + bold(languages[i] ) +translated_phrases[i],insert_sound(deck_name + "phrase" + str(i)) + phrases[i],""])
-          if cardtype=="v":
-              note =create_note(["",insert_sound(deck_name + "word" + str(i)) + color + bold(words[i])+  " == " + translated_words[i],
-                                insert_sound(deck_name + "phrase" + str(i))  + color+ bold(words[i]) + ". " +phrases[i]])
-          if cardtype=="w":
-              note = create_note([color + bold(languages[i])+translated_phrases[i], phrases[i],insert_sound(deck_name+"phrase"+str(i))])
-          if cardtype=="p":
-              note = create_note([color + bold(phrases[i]),insert_sound(deck_name + "pronunciation" + str(i))," ", ""])
+              note =create_note([f"{color}{bold(langs[i])}{translated_phrases[i]}",
+                                 f"{insert_sound(f'{deck_name}phrase{i}')}{phrases[i]}",""])
+          elif cardtype=="v":
+              note =create_note(["",f"{insert_sound(f'{deck_name}word{i}')}\
+                                 {color}{bold(words[i])} == {translated_words[i]}",
+                                f"{insert_sound(f'{deck_name}phrase{i}')}{color}\
+                                  {bold(words[i])}. {phrases[i]}"])
+          elif cardtype=="w":
+              note = create_note([f"{color}{bold(langs[i])}{translated_phrases[i]}", 
+                                  phrases[i],
+                                  insert_sound(f"{deck_name}phrase{i}")])
+          elif cardtype=="p":
+              note = create_note([f"{color}{bold(phrases[i])}",
+                                  insert_sound(f"{deck_name}pronunciation{i}"),
+                                  " ", ""])
           deck.add_note(note)
-        except:pass
+        except:...
 
 
-  media_files=[audio_path+"//"+j for j in os.listdir(audio_path)]
-  genanki.Package(deck,media_files=media_files).write_to_file(deck_name +'.apkg')
+  media_files=[f"{audio_path}//{j}" for j in listdir(audio_path)]
+  genanki.Package(deck,media_files=media_files).write_to_file(f"{deck_name}.apkg")
 
-  deltat=time.time() -t_o
-  print(f"Congratulations, {len(phrases)} flashcards in {round(deltat/60,1)} minutes! {round(60*len(phrases)/deltat,1)} flashcards per minute")
+  deltat=time() -t_o
+  print(f"Congratulations, {len(phrases)} flashcards in {deltat/60:.1f} 
+        minutes! {60*len(phrases)/deltat:1f} flashcards per minute")

d9bf2161c5d69c3dde71fe15bcfd35c7befb0b27
viniciusdutra314 <dutravinisousa2@usp.br>
2023-12-25 12:03:47 -0300

    removed command-line-interface
    the future of this project is as an Anki Add-on with a PyQt6 interface

--- tests/test_vocabulary.py
+++ tests/test_vocabulary.py
@@ -13,12 +13,12 @@ def async_audio():
     global cardtype,path
     for i,table in enumerate(sorted(listdir(path))):
         phrases,words,langs,deck_name=data_from_starting_table('v',path+'//'+table)
-        create_audios(cardtype,langs,phrases,'tests//audio_tests',f'exemplo{i}',words)
+        generate_audios(cardtype,langs,phrases,'tests//audio_tests',f'exemplo{i}',words)
 
 def test_async_translate():
     global cardtype,num_lines,path
     chosen_lang='pt'
     for i,table in enumerate(sorted(listdir(path))):
         phrases,words,langs,deck_name=data_from_starting_table('v',path+'//'+table)
-        trans_phrases,trans_words=translate_phrases_n_words(cardtype,langs,chosen_lang,phrases,words)
+        trans_phrases,trans_words=generate_translation(cardtype,langs,chosen_lang,phrases,words)
         assert len(trans_phrases)==len(trans_words)==len(langs)==num_lines[i], 'Incompatible number of elements'
